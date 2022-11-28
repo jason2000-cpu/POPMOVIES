9e5df921cde2b46804c6c56624b2456f563a0041
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-11-28 23:36:26 +0300

    added modal login/signup form

--- app.js
+++ app.js
@@ -33,3 +33,17 @@ ball.addEventListener("click", () => {
   });
   ball.classList.toggle("active");
 });
+
+
+
+//Login and Signup
+
+// Get the modal
+const  modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if(event.target == modal) {
+    modal.style.display = "none";
+  }
+}
